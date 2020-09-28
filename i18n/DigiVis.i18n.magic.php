7893b9ff62a041387a35b76645a66b7a58c61fc8
Manfred Moosleitner <manfred.moosleitner@uibk.ac.at>
2020-09-28 11:31:51 +0200

    updates translation files

--- i18n/DigiVis.i18n.magic.php
+++ i18n/DigiVis.i18n.magic.php
@@ -6,6 +6,5 @@ $magicWords['en'] = [
 	'layer2display' => [0, 'layer2display'],
 	'layer2annotate' => [0, 'layer2annotate'],
 	'layer3display' => [0, 'layer3display'],
-	'testitest' => [0, 'testitest'],
 	'a2t' => [0, 'a2t']
 ];
